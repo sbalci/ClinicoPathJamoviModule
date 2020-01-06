cf28ca3aa13962c34c2277eb78815780de31a3cc
SERDAR BALCI <drserdarbalci@gmail.com>
2020-01-06 23:59:06 +0300

    updated on 2020-01-06 23:59:06

--- R/survival.b.R
+++ R/survival.b.R
@@ -7,49 +7,49 @@ survivalClass <- if (requireNamespace('jmvcore')) R6::R6Class(
     inherit = survivalBase,
     private = list(
         .run = function() {
-            
+
             if (length(self$options$factor) + length(self$options$outcome) + length(self$options$overalltime) < 3)
                 return()
 
-                        
+
             mydata <- self$data
-            
+
             myoveralltime <- self$options$overalltime
-            
+
             myoveralltime <- jmvcore::toNumeric(self$data[[myoveralltime]])
-            
+
             myfactor <- self$options$factor
-            
+
             myfactor <- self$data[[myfactor]]
-            
+
             myoutcome <- self$options$outcome
-          
+
             myoutcome <- self$data[[myoutcome]]
-            
+
             km_fit <- survival::survfit(survival::Surv(myoveralltime, myoutcome) ~ myfactor, data = mydata)
-            
+
             surv_sum <- survminer::surv_summary(km_fit, data = mydata)
-            
+
             km_fit_median_df <- summary(km_fit)[["table"]]
-            
+
             # km_fit_median_df <- km_fit_median_df$table
-            
-            km_fit_median_df <- 
+
+            km_fit_median_df <-
                 as.data.frame(km_fit_median_df) %>%
                 tibble::rownames_to_column()
-            
-            
-            
+
+
+
             # km_fit <- survival::survfit(formula = survival::Surv(myoveralltime, myoutcome) ~ myfactor, data = mydata)
-            # 
+            #
             # # s <- survival::survfit(formula=survival::Surv(elapsed, event) ~ group, data=data)
-            # 
+            #
             # km_fit_Table <- summary(km_fit)$table
-            # 
+            #
             # # sTable <- summary(s)$table
-            # 
+            #
             # st <- self$results$summary
-            # 
+            #
             # for (i in seq_len(nrow(km_fit))) {
             #     if (nrow(km_fit) == 1)
             #         g <- km_fit_Table
@@ -61,7 +61,7 @@ survivalClass <- if (requireNamespace('jmvcore')) R6::R6Class(
             #     median <- g['median']
             #     mean <- g['*rmean']
             #     prop <- nevents / n
-            #     
+            #
             #     # st$setRow(rowNo=i, list(
             #     #     censored=ncensor,
             #     #     events=nevents,
@@ -70,14 +70,14 @@ survivalClass <- if (requireNamespace('jmvcore')) R6::R6Class(
             #     #     median=median,
             #     #     mean=mean))
             # }
-            # 
+            #
             # st$setStatus('complete')
-            # 
+            #
             # self$results$summary2$setContent(st)
-            # 
+            #
             # self$results$summary$setContent(summary)
-            
-            
+
+
 
             # km_fit_median_definition <-
 
@@ -91,11 +91,11 @@ survivalClass <- if (requireNamespace('jmvcore')) R6::R6Class(
             #     ) %>%
             #     dplyr::select(description) %>%
             #     dplyr::pull()
-                        
-            
+
+
             # sTable <- summary(km_fit)$table
             # st <- self$results$summary
-            # 
+            #
             # for (i in seq_len(nrow(km_fit))) {
             #     if (nrow(km_fit) == 1)
             #         g <- sTable
@@ -107,7 +107,7 @@ survivalClass <- if (requireNamespace('jmvcore')) R6::R6Class(
             #     median <- g['median']
             #     mean <- g['*rmean']
             #     prop <- nevents / n
-            #     
+            #
             #     st$setRow(rowNo=i, list(
             #         censored=ncensor,
             #         events=nevents,
@@ -116,75 +116,75 @@ survivalClass <- if (requireNamespace('jmvcore')) R6::R6Class(
             #         median=median,
             #         mean=mean))
             # }
-            # 
+            #
             # st$setStatus('complete')
-            # 
-            # 
+            #
+            #
             # results1 <- st
-            
-            
-            
-            results1 <- surv_summary(km_fit, data = mydata)
-            
+
+
+
+            results1 <- survminer::surv_summary(km_fit, data = mydata)
+
             results2 <- summary(km_fit)$table
-            
+
             results3 <- surv_sum
-            
+
             results4 <- km_fit_median_df$table
-            
+
             results5 <- capture.output(km_fit)
-            
+
             self$results$text1$setContent(results1)
-            
+
             self$results$text2$setContent(results2)
 
             self$results$text3$setContent(results3)
 
             self$results$text4$setContent(results4)
-            
+
             self$results$text5$setContent(results5)
-            
-            
-            
+
+
+
             # formulaL <- jmvcore::format('({a} , {b})', a = myoveralltime, b = formulaL2)
             # formulaL <- paste("(", formulaL1, "," ,formulaL2, ")")
 
             # formulaR <- jmvcore::constructFormula(terms = self$options$factor)
             # formulaR <- jmvcore::constructFormula(terms = "myfactor")
-            
+
             # formula <- jmvcore::composeFormula(lht = formulaL, rht = formulaR)
             # formula <- paste(formulaL, '~', formulaR, ', data = self$data')
-            
+
             # formula <- as.formula(formula)
-            
-            
+
+
             # km_fit <- survival::survfit(survival::Surv(formula))
-            
-            
+
+
             # `self$data` contains the data
             # `self$options` contains the options
             # `self$results` contains the results object (to populate)
 
         }
-        
+
         # ,
-        # 
-        # 
+        #
+        #
         # .plot=function(image, theme, ggtheme, ...){
-        #     
-        #     
-        # 
+        #
+        #
+        #
         #     plot(km_fit)
-        #     
-        #     
-        #     
-        #     
+        #
+        #
+        #
+        #
         # }
-        
-        
-        
-        
-        
-        
+
+
+
+
+
+
         )
 )
