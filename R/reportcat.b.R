b827ce4de6bff128bb6a4bd33f60d7d5da977ee2
SERDAR BALCI <drserdarbalci@gmail.com>
2020-07-03 22:50:03 +0300

    # ClinicoPath 0.0.2.0019
    - rewrote summary of categorical values to decrease dependencies

--- R/reportcat.b.R
+++ R/reportcat.b.R
@@ -89,28 +89,28 @@ reportcatClass <- if (requireNamespace('jmvcore')) R6::R6Class(
             myvars <- unlist(myvars)
 
 
-            myreport <- mydata %>%
-                select(myvars) %>%
-                report::report(.,
-                               median = FALSE,
-                               centrality = TRUE,
-                               dispersion = TRUE,
-                               range = TRUE,
-                               distribution = FALSE,
-                               levels_percentage = FALSE,
-                               n_entries = 3,
-                               missing_percentage = FALSE
-            #                    median = med,
-            #                    centrality = cent,
-            #                    dispersion = disp,
-            #                    range = ran,
-            #                    distribution = distr,
-            #                    levels_percentage = lev,
-            #                    n_characters = n_ch,
-            #                    missing_percentage = mis
-                               )
-
-            results1 <- myreport
+            # myreport <- mydata %>%
+            #     select(myvars) %>%
+            #     report::report(.,
+            #                    median = FALSE,
+            #                    centrality = TRUE,
+            #                    dispersion = TRUE,
+            #                    range = TRUE,
+            #                    distribution = FALSE,
+            #                    levels_percentage = FALSE,
+            #                    n_entries = 3,
+            #                    missing_percentage = FALSE
+            # #                    median = med,
+            # #                    centrality = cent,
+            # #                    dispersion = disp,
+            # #                    range = ran,
+            # #                    distribution = distr,
+            # #                    levels_percentage = lev,
+            # #                    n_characters = n_ch,
+            # #                    missing_percentage = mis
+            #                    )
+            #
+            # results1 <- myreport
 
 
 
@@ -119,6 +119,67 @@ reportcatClass <- if (requireNamespace('jmvcore')) R6::R6Class(
             #     dplyr::filter(na > 0)
 
 
+            # for (fac in facs)
+            #     data[[fac]] <- as.factor(data[[fac]])
+
+
+
+            # catsummary function
+            catsummary <- function(myvar) {
+
+
+
+
+                leng <- length(mydata[[myvar]])
+
+                miss <- sum(is.na(mydata[[myvar]]))
+
+                valid <- leng - miss
+
+                nlev <- nlevels(as.factor(mydata[[myvar]]))
+
+                summar <- summary(as.factor(mydata[[myvar]])) %>%
+                    as.table() %>%
+                    tibble::as_tibble(.name_repair = "unique") %>%
+                    dplyr::filter(.[[1]] != "NA's") %>%
+                    dplyr::arrange(dplyr::desc(n))
+
+                summar$validtotal <- valid
+
+                description <- summar %>%
+                    dplyr::mutate(
+                        percent = n/validtotal
+                    ) %>%
+                    dplyr::mutate(
+                        description = glue::glue(
+                            "{...1}: n = {n}, {scales::percent(percent)}. "
+                        )
+                    ) %>%
+                    dplyr::select(description) %>%
+                    dplyr::pull(.)
+
+
+                sentence1 <- paste0(
+                    myvar, " has ", leng, " observations and ", nlev, " levels. "
+                )
+
+                sentence3 <- paste0("There are ", miss, " missing values.")
+
+                sentence <- c(
+                    sentence1,
+                    unlist(description),
+                    sentence3
+                )
+
+                return(paste0(sentence, collapse = ""))
+
+            }
+
+
+
+            results <- purrr::map(.x = myvars, .f = catsummary)
+
+            results1 <- unlist(results)
 
             self$results$text$setContent(results1)
 
