5e405630b40337cdf5b28e7dfb1de898df0cf5e9
Serdar <drserdarbalci@gmail.com>
2021-02-16 12:06:30 +0300

    added ggvenn

--- R/data.R
+++ R/data.R
@@ -1,29 +1,3 @@
-#' @title Melanoma
-#'
-#' @description melanoma dataset obtained from boot.
-#' @usage data(melanoma)
-#' @format A data frame
-#' @source boot package
-"melanoma"
-
-#' @title colon
-#'
-#' @description colon cancer dataset.
-#'
-#' @format A data frame
-#' @usage data(colon)
-#' @source survival package
-"colon"
-
-#' @title BreastCancer
-#'
-#' @description Wisconsin Breast Cancer Database dataset.
-#'
-#' @format A data frame
-#' @usage data(BreastCancer)
-#' @source mlbench package
-"BreastCancer"
-
 #' @title histopathology
 #'
 #' @description Fake histopathology research data.
