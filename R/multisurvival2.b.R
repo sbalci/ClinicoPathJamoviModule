6976310fe731baf72a7028e4ecba949c6c19a3ce
sbalci <drserdarbalci@gmail.com>
2023-12-03 23:53:42 +0300

    WIP

---
+++ R/multisurvival2.b.R
@@ -0,0 +1,15 @@
+
+# This file is a generated template, your changes will not be overwritten
+
+multisurvival2Class <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class(
+    "multisurvival2Class",
+    inherit = multisurvival2Base,
+    private = list(
+        .run = function() {
+
+            # `self$data` contains the data
+            # `self$options` contains the options
+            # `self$results` contains the results object (to populate)
+
+        })
+)
