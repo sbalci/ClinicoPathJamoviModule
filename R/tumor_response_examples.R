4268e5493097b2028ab9323473a7f8f185652420
sbalci <drserdarbalci@gmail.com>
2025-01-19 20:57:14 +0300

    up

---
+++ R/tumor_response_examples.R
@@ -0,0 +1,25 @@
+#' Example Tumor Response Datasets
+#'
+#' Example datasets demonstrating different scenarios for tumor response analysis
+#'
+#' @format A list containing 4 data frames:
+#' \describe{
+#'   \item{raw_with_time}{Raw tumor measurements with time points (20 obs. of 3 variables)}
+#'   \item{raw_no_time}{Raw tumor measurements without time (10 obs. of 2 variables)}
+#'   \item{percent_with_time}{Pre-calculated response percentages with time (20 obs. of 3 variables)}
+#'   \item{percent_no_time}{Pre-calculated response percentages without time (10 obs. of 2 variables)}
+#' }
+#' @details
+#' Each dataset represents a different scenario:
+#' \itemize{
+#'   \item raw_with_time: Longitudinal raw measurements
+#'   \item raw_no_time: Single timepoint raw measurements
+#'   \item percent_with_time: Longitudinal percentage changes
+#'   \item percent_no_time: Single timepoint percentage changes
+#' }
+#'
+#' @examples
+#' data(tumor_response_examples)
+#' head(tumor_response_examples$raw_with_time)
+#' head(tumor_response_examples$percent_no_time)
+"tumor_response_examples"
