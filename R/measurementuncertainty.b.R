de3e84d700dfbbf1ff367ed546eda12bd60387d6
sbalci <drserdarbalci@gmail.com>
2025-08-24 11:13:48 +0300

    Add measurement uncertainty and method validation modules
    Introduced new R6 classes, options, and functions for measurement uncertainty estimation (ISO 15189) and laboratory method validation protocols. Updated NAMESPACE and jamovi YAML files to register the new modules. Added corresponding documentation files for both features.

---
+++ R/measurementuncertainty.b.R
@@ -0,0 +1,15 @@
+
+# This file is a generated template, your changes will not be overwritten
+
+measurementuncertaintyClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class(
+    "measurementuncertaintyClass",
+    inherit = measurementuncertaintyBase,
+    private = list(
+        .run = function() {
+
+            # `self$data` contains the data
+            # `self$options` contains the options
+            # `self$results` contains the results object (to populate)
+
+        })
+)
---
+++ R/referenceintervals.b.R
@@ -0,0 +1,15 @@
+
+# This file is a generated template, your changes will not be overwritten
+
+referenceintervalsClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class(
+    "referenceintervalsClass",
+    inherit = referenceintervalsBase,
+    private = list(
+        .run = function() {
+
+            # `self$data` contains the data
+            # `self$options` contains the options
+            # `self$results` contains the results object (to populate)
+
+        })
+)
